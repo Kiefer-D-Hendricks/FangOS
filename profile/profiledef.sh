0b627e14d79bb831aee8a013b5edf25929fd4edc
Kiefer D. Hendricks <kieferhendricks2@gmail.com>
2026-09-08 22:48:03 -0500

    Still screwing with the Desktop

--- profile/profiledef.sh
+++ profile/profiledef.sh
@@ -12,7 +12,7 @@ bootmodes=('bios.syslinux'
            'uefi.systemd-boot')
 pacman_conf="pacman.conf"
 airootfs_image_type="squashfs"
-airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
+airootfs_image_tool_options=('-comp' 'zstd' '-Xcompression-level' '1')
 bootstrap_tarball_compression=('zstd' '-c' '-T0' '--auto-threads=logical' '--long' '-19')
 file_permissions=(
   ["/etc/shadow"]="0:0:400"
@@ -24,5 +24,6 @@ file_permissions=(
   ["/usr/local/bin/livecd-sound"]="0:0:755"
   ["/home/fang"]="1000:1000:0755"
   ["/home/fang/.config"]="1000:1000:0755"
+  ["/home/fang/.config/kdeglobals"]="1000:1000:0644"
   ["/home/fang/.config/plasma-org.kde.plasma.desktop-appletsrc"]="1000:1000:0644"
 )
